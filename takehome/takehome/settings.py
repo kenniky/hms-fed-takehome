45131b2952fd820b6f843ef86339e3638feb1401
kenwang <kenwang@umich.edu>
2023-01-14 17:33:37 -0500

    simple page

--- takehome/takehome/settings.py
+++ takehome/takehome/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'takehome.apps.HmstakehomeConfig',
+    'hmstakehome.apps.HmstakehomeConfig',
 ]
 
 MIDDLEWARE = [
--- takehome/takehome/urls.py
+++ takehome/takehome/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('takehome.urls')),
+    path('', include('hmstakehome.urls')),
 ]
