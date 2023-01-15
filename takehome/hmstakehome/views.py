af4644b281732a6ae44d0ddce363218a02b69109
kenwang <kenwang@umich.edu>
2023-01-14 23:35:58 -0500

    basic templating success

--- takehome/hmstakehome/views.py
+++ takehome/hmstakehome/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def class_view(request, class_slug):
+    return render(request, "class.html", context={'class_slug': class_slug})
